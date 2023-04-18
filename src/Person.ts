12c9e7e96e9503ccc5c98d925f74d560092cfbd1
Vivien Le Duc <leducvivien@gmail.com>
2023-04-19 00:27:00 +0200

    first commit

---
+++ src/Person.ts
@@ -0,0 +1,11 @@
+/**
+ * Author and Contributors type.
+ */
+export default interface Person {
+  /** The person's name. */
+  name: string;
+  /** The person's email. */
+  email?: string;
+  /** The person's url. */
+  url?: string;
+}
